d87fd543753c9e1985a8a128514dfe4dea1d96f4
coding@justlike.pro <coding@justlike.pro>
2016-10-03 19:42:30 +0500

    всё работает

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,31 @@
-
+import os.path, collections, re
 
 def load_data(filepath):
-    pass
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data_file = current_dir + '\\' + filepath
+    file = open(data_file, "r", encoding="utf-8")
+
+    return file.read().lower()
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(textfile):
+    text = re.findall(r'\w+', textfile)
+    counter = collections.Counter(text)
+    text = counter.most_common(10)
 
+    return text
 
 if __name__ == '__main__':
-    pass
+
+    filepath = ''
+
+    while not os.path.isfile(filepath):
+        try:
+            words = get_most_frequent_words(load_data(input('Введите название текстового файла: ')))
+            for word in words:
+                print(word[0],word[1])
+            exit()
+
+        except FileNotFoundError:
+            print('Данного файла не существует в директории с программой')
+
