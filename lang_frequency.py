3a6a11629ad836ec68587c92e50dd86676295e17
arseni-baranov <arseni-baranov@yandex.ru>
2016-10-09 21:36:09 +0500

    Внесено несколько важных правок

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,29 +1,28 @@
 import os.path, collections, re
 
-def load_data(filepath):
+def load_data(file_path):
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data_file = current_dir + '\\' + filepath
-    file = open(data_file, "r", encoding="utf-8")
+    data_file = current_dir + '\\' + file_path
+    with open(data_file, "r", encoding="utf-8") as data_file:
+        return data_file.read().lower()
 
-    return file.read().lower()
 
-
-def get_most_frequent_words(textfile):
-    text = re.findall(r'\w+', textfile)
+def get_most_frequent_words(text_file, top_size):
+    text = re.findall(r'\w+', text_file)
     counter = collections.Counter(text)
-    text = counter.most_common(10)
+    text = counter.most_common(top_size)
 
     return text
 
 if __name__ == '__main__':
 
-    filepath = ''
+    file_path = None
 
-    while not os.path.isfile(filepath):
+    while file_path is None:
         try:
-            words = get_most_frequent_words(load_data(input('Введите название текстового файла: ')))
-            for word in words:
-                print(word[0],word[1])
+            words = get_most_frequent_words(load_data(input('Введите название текстового файла: ')), 10)
+            for word, word_frequency in words:
+                print(word, word_frequency)
             exit()
 
         except FileNotFoundError:
